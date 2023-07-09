9892e5c7ffccb83ade84e96407231061be41e2d6
fagner <100000573+fagnerdk@users.noreply.github.com>
2023-07-09 13:32:53 -0300

    0.1

--- js/online.js
+++ js/online.js
@@ -1,13 +1,14 @@
+
 function onFunction() {
-    var a= document.getElementById("lane");
-     a.innerHTML="online"
-     a.style.backgroundColor='lime'  
+     var b= document.getElementById("lane");
+     b.innerHTML=" offline"
+     b.style.backgroundColor='red' ;
 } 
 onFunction()  
 function offFunction() {
- var b= document.getElementById("lane");
- b.innerHTML=" offline"
- b.style.backgroundColor='red' ;
+ var a= document.getElementById("lane");
+ a.innerHTML="online"
+ a.style.backgroundColor='lime' 
        
 }
 offFunction()  
