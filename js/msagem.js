67d18df3d698352141cafc6501a0bbdd7a3bb5b9
fagner <100000573+fagnerdk@users.noreply.github.com>
2023-12-18 21:38:14 -0300

    Update msagem.js

--- js/msagem.js
+++ js/msagem.js
@@ -1,15 +1,40 @@
+
+const arry = [];
+
 function mymsg() {
-  var x = document.querySelector('[id="testo1"]').value;
+  var t1 = document.getElementById("testo1").value;
+  const nome = {
+    nome: `${t1}`,
+  };
+  const mysql = {
+    nome: `${nome.nome}`,
+  };
+  const tablemysql = [];
 
-  /*************/
+  arry.push("<li>" + mysql.nome  +"</li>" );
 
-  const para = document.createElement("p");
-  const node = document.createTextNode(x);
-  para.appendChild(node);
-  const element = document.getElementById("msg");
-  element.appendChild(para);
+  for (var i = 0; i < arry.length; i++) {
+    arry[i] 
+    document.getElementById("t").innerHTML =  arry  
+  }
+  
+  console.table(arry);
 }
-/**** */
-function mydelete() {
-  var x = document.querySelector('[id="testo1"]').value;
+
+function mydelete(){
+  
+  var x = document.getElementById("t").innerHTML = arry.pop()
+  
 }
+setInterval(()=>{
+  if(arry.length > 8){
+   var x = document.getElementById("t").innerHTML = arry.shift()
+  }
+ },90)
+
+setInterval(()=>{
+  document.getElementById("t").innerHTML = arry
+},90)
+
+
+
